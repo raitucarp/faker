c5775691626f77e181c43e6c029cd52e1d39f10e
Raitucarp <ribhararnus.pracutiar@gmail.com>
2016-03-01 14:59:19 +0700

    add hacker faker method

---
+++ hacker.go
@@ -0,0 +1,53 @@
+package faker
+
+type Hacker struct {
+	Abbreviation string
+	Adjective    string
+	Noun         string
+	Verb         string
+	IngVerb      string
+	Phrase       string
+}
+
+func (h *Hacker) Abbreviation_() string {
+	abbr := getData("Hacker", "Abbreviation")
+	h.Abbreviation = sample(abbr)
+	return h.Abbreviation
+}
+
+func (h *Hacker) Adjective_() string {
+	adjective := getData("Hacker", "Adjective")
+	h.Adjective = sample(adjective)
+	return h.Adjective
+}
+
+func (h *Hacker) Noun_() string {
+	noun := getData("Hacker", "Noun")
+	h.Noun = sample(noun)
+	return h.Noun
+}
+
+func (h *Hacker) Verb_() string {
+	verb := getData("Hacker", "Verb")
+	h.Verb = sample(verb)
+	return h.Verb
+}
+
+func (h *Hacker) IngVerb_() string {
+	ingverb := getData("Hacker", "IngVerb")
+	h.IngVerb = sample(ingverb)
+	return h.IngVerb
+}
+
+func (h *Hacker) Phrase_() string {
+	phrase := getData("Hacker", "Phrase")
+	template := sample(phrase)
+
+	result, err := Fake(template)
+	if err != nil {
+		result = ""
+	}
+
+	h.Phrase = result
+	return h.Phrase
+}
