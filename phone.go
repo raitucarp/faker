c839ab36b8d43f64c6a6adbb11c9610a834be0ed
Raitucarp <ribhararnus.pracutiar@gmail.com>
2016-03-02 18:34:50 +0700

    add phone type

---
+++ phone.go
@@ -0,0 +1,45 @@
+package faker
+
+import "reflect"
+
+type Phone struct {
+	Number string
+	Format string
+}
+
+/*
+ self.phoneNumber = function (format) {
+      format = format || faker.phone.phoneFormats();
+      return faker.helpers.replaceSymbolWithNumber(format);
+  };
+
+  // FIXME: this is strange passing in an array index.
+  self.phoneNumberFormat = function (phoneFormatsArrayIndex) {
+      phoneFormatsArrayIndex = phoneFormatsArrayIndex || 0;
+      return faker.helpers.replaceSymbolWithNumber(faker.definitions.phone_number.formats[phoneFormatsArrayIndex]);
+  };
+
+  self.phoneFormats = function () {
+    return faker.random.arrayElement(faker.definitions.phone_number.formats);
+  };
+
+*/
+
+func (phone *Phone) Number_(params ...interface{}) string {
+	format := phone.Format_()
+
+	types := typeof(params...)
+	if len(types) >= 1 && types[0] == reflect.String {
+		format = params[0].(string)
+	}
+
+	phone.Number = replaceSymbolsWithNumber(format, '#')
+
+	return phone.Number
+}
+
+func (phone *Phone) Format_() string {
+	format := getData("Phone", "PhoneFormat")
+	phone.Format = sample(format)
+	return phone.Format
+}
