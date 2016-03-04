bc524b73b0b20c800ebabeba3464095177b0f2ed
Raitucarp <ribhararnus.pracutiar@gmail.com>
2016-03-04 15:08:05 +0700

    add initial documentation for godoc

---
+++ doc.go
@@ -0,0 +1,10 @@
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package faker is just another random data generator for go based on faker.js.
+
+To generate random data for testing usually hard. With faker it's easy to create random data that...
+*/
+package faker
